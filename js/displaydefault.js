96b0ab2a9e4f9253dc769dedf3179a5d1d1fbc2b
Grant TF <grant@tarrant-fisher.co.uk>
2019-03-16 10:03:53 +0000

    displaydefault.js for some reason

---
+++ js/displaydefault.js
@@ -0,0 +1,8 @@
+function displaydefault() {
+    detailsShow();
+    newMembForm.css("display", "none");
+    existingMembForm.css("display", "none");
+    paymentPanel.css("display", "none");
+    divLogin.css("display", "none");
+    divMain.css("display", "block");
+}
