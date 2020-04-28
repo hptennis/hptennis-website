2df34e10920d67bed64a37d079752ebb0e328f74
gtfisher <grant@tarrant-fisher.co.uk>
2020-04-28 18:47:56 +0100

    add firebase to members grid WIP

--- fb/members.js
+++ fb/members.js
@@ -1,60 +1,140 @@
 
 
-// var firebaseConfig = {
-//   apiKey: "AIzaSyACL7vD3at2aFbtj985_3wSphYvW-rIG3E",
-//   authDomain: "membershipdb.firebaseapp.com",
-//   databaseURL: "https://membershipdb.firebaseio.com",
-//   projectId: "membershipdb",
-//   storageBucket: "membershipdb.appspot.com",
-//   messagingSenderId: "831601336894",
-//   appId: "1:831601336894:web:6c82a92adaa5fff1"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// // Firebase Database Reference and the child
-// const dbRef = firebase.database().ref();
-// const membersRef = dbRef.child('members');
-
-// // Firebase Variables
-// var auth = firebase.auth();
+var dbRef
+var membersRef
+var count = 0;
 
 
-var app_fireBase = {};
-var dbRef = {};
-var membersRef = {};
-(function() { 
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyAon8MXKY0n6PPbf3-emYLRFuqUk9SUKyo",
-  //   authDomain: "authtry-45433.firebaseapp.com",
-  //   databaseURL: "https://authtry-45433.firebaseio.com",
-  //   projectId: "authtry-45433",
-  //   storageBucket: "",
-  //   messagingSenderId: "933586310402"
-  // };
+var callbackAdded = function(snap) {
+    
+	// console.log("callbackAdded");
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAon8MXKY0n6PPbf3-emYLRFuqUk9SUKyo",
-    authDomain: "authtry-45433.firebaseapp.com",
-    databaseURL: "https://authtry-45433.firebaseio.com",
-    projectId: "authtry-45433",
-    storageBucket: "authtry-45433.appspot.com",
-    messagingSenderId: "933586310402",
-    appId: "1:933586310402:web:6678a4f774bf3d1cbb5e1b"
-  };
-  firebase.initializeApp(firebaseConfig);
+	var row = snap.val();
+	row.key = snap.key
 
-  app_fireBase = firebase;
+	// console.log("row from db:", snap.key);
+	// console.log("row from db:", row);
+	row.key = snap.key
+	// console.log("row from db:", row);
+	var table = $('#example').DataTable();
+	
+	// table.row.add(row).draw();
+	table.row.add(row);
 
-   dbRef = app_fireBase.database().ref();
-   membersRef = dbRef.child('members');
+	
 
 
-})()
+	//var dataRow = buildLicenseRow(snap.key, snap.val());
+	// var license = new License(row.notValidBefore,row.notValidAfter ,snap.key,row.customer,row.serial, row.hcf,row.maintDate,row.state, row.version,row.s2s,row.wrm);
+
+	//  var table = $('#example').DataTable ();
+	//  table.rows.add([row]).draw();
+
+};
+
+var callbackChanged = function(snap) {
+	console.log("callbackChanged");
+	
+	// var dataRow = buildLicenseRow(snap.key, snap.val());
+	// var table = $('#example').DataTable ();
+	// table.draw();
+	// location.reload();
+	// table.rows.add([dataRow]).draw();
+
+};
+
+function firebase_init(){
+
+try {
+            
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyACL7vD3at2aFbtj985_3wSphYvW-rIG3E",
+        authDomain: "membershipdb.firebaseapp.com",
+        databaseURL: "https://membershipdb.firebaseio.com",
+        projectId: "membershipdb",
+        storageBucket: "membershipdb.appspot.com",
+        messagingSenderId: "831601336894",
+        appId: "1:831601336894:web:6c82a92adaa5fff1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    // firebase.analytics();
+
+	var database = firebase.database();
+
+    dbRef = firebase.database().ref();
+	membersRef = dbRef.child('members');
+
+	firebase.database().ref('/members/').once('value').then(function(snapshot) {
+
+		  snapshot.forEach(function(doc){
+			  console.log(doc.val())
+			  var row = doc.val();
+			  row.key = doc.key
+			  var table = $('#example').DataTable();
+	
+			  // table.row.add(row).draw();
+			  table.row.add(row);
+
+			
+		   });
+		
+		   table.draw();
+		   membersRef.on('child_added', callbackAdded);
+    	   membersRef.on('child_changed', callbackChanged);
+	});
+
+	// database.collection("members")
+    // .get()
+    // .then(function(querySnapshot) {
+    //     querySnapshot.forEach(function(doc) {
+    //         // doc.data() is never undefined for query doc snapshots
+    //         console.log(doc.id, " => ", doc.data());
+    //     });
+    // })
+    // .catch(function(error) {
+    //     console.log("Error getting documents: ", error);
+    // });
 
 
-readMembersData(); 
+	
+	// membersRef.on('child_added', callbackAdded);
+    // membersRef.on('child_changed', callbackChanged);
+
+    // readMembersData();
+
+
+//   let app = firebase.app();
+//   let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+//   document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+
+    
+} catch (e) {
+  console.error("firebase load error",e);
+//   document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+}
+
+}
+
+  
+function writeEntry(key, data)
+{
+	
+	firebase.database().ref('members/' +key).set({
+		data
+	  }, function(error) {
+		if (error) {
+		 return "update failed";
+
+		} else {
+		 return "ok";
+		  
+		}
+	  });
+}
+
+// readMembersData(); 
   
 
   function showHide(show, elemId) {
@@ -73,17 +153,41 @@ readMembersData();
 // --------------------------
 function readMembersData() {
 
+	console.log("readMembersData");
 	membersRef.on("value", snap => {
 
+	 	console.log("got value");
+	 	
+
+		 var row = snap.val();
+		 
+		 snap.forEach(childSnap => {
+
+			let key = childSnap.key,
+			value = childSnap.val();
+			
+  			
+		 });
+		 
+
+    //     console.log("row from db:", snap.key);
+    //     console.log("row from db:", row);
+
+    //     //var dataRow = buildLicenseRow(snap.key, snap.val());
+    //     // var license = new License(row.notValidBefore,row.notValidAfter ,snap.key,row.customer,row.serial, row.hcf,row.maintDate,row.state, row.version,row.s2s,row.wrm);
+	// 	var member = new Membership(row.firstName,row.lastName);
+    //     var table = $('#example').DataTable ();
+    //     table.rows.add([member]).draw();
+
 	
 
-		snap.forEach(childSnap => {
+		// snap.forEach(childSnap => {
 
 	
- 		});
+ 		// });
 
 
-	})
+	 });
 
 }
 
@@ -110,73 +214,150 @@ function userClicked(e) {
 		});
 	
 
-}
-
-
-
-
+} 
 
 // --------------------------
 // ADD
 // --------------------------
 
-const addMemberBtnUI = document.getElementById("addMemberBtn");
-addMemberBtnUI.addEventListener("click", addMemberClicked)
+// const addMemberBtnUI = document.getElementById("addMemberBtn");
+// addMemberBtnUI.addEventListener("click", addMemberClicked)
 
 
 
-function addMemberClicked() {
+function tempAddMemberBtn()
+{
+	console.log("tempAddMemberBtn");
+	// gtf = {"2019-03-29T17:30:47.927Z","1104","Grant,Tarrant-Fisher","27 Decoy Drive","","BN22 0AD","07986-240984","grant@tarrant-fisher.co.uk","yes","adult","online",null}
+	var x = {
+		"id": "3",
+		"name": "Ashton Cox",
+		"position": "Junior Technical Author",
+		"salary": "$86,000",
+		"start_date": "2009/01/12",
+		"office": "San Francisco",
+		"extn": "1562"
+	  };
 
-	console.log("addMemberClicked");
+	var table2 = $('#example2').DataTable();  
+	var d  = table2.data();
+    console.log( 'There are'+d.length+' row(s) of data in this table' ); 
 
-	// const membersRef = dbRef.child('members');
+	// d.push(x);
+	//table2.row.add(x).draw();
 
-	const addUserInputsUI = document.getElementsByClassName("user-input");
+	var rowInd = -1;
+	for(var i = 0; i < d.length; i++) {
+		if (d[i].id == '3') {
+			rowInd = i;
+			break;
+		}
+	}
 
-	var form = $('#memberform');
+	if (rowInd >0) {
+		d[rowInd].name = "Fred";
+		table2.rows().invalidate();
+	}
+	else
+	{
+		table2.row.add(x).draw();	
+	}
 
-	var formData = form.serializeArray();
+    console.log( 'There are'+table2.data().length+' row(s) of data in this table' ); 
 
-	var membership = new Membership($('#id01firstname').val(),
-									$('#id01lastname').val(),
-									$('#id01address1').val(),
-									$('#id01address2').val(),
-									$('#id01postcode').val(),
-									$('#id01email').val(),
-									$('#id01phone').val(),
-									$('#id01mtype').val(),
-									$('#id01endDate').val());
 
-	console.log ("wrtie member to db:", JSON.stringify (membership));
+	// var t = $('#example2').DataTable();
+	// t.draw( true );
+	// location.reload();
+	// Invalidate all rows and redraw
+	// table2.rows().invalidate().draw();
+
+}
+
+function importData()
+{
+	alert ("importData")
+	var myList;
+	$.getJSON('members-20200419.json')
+		.done(function (data) {
+		myList = data;
+
+		myList.forEach(importItem);
+
+
+
+	});
+
+}
+
+function importItem(item, index)
+{
+	writeMemberData(item)
+
+}
+
+function writeMemberData(data) {
+    // alert ("send to firebase");
+    // firebase.database().ref('licenses/' + key).set({
+        //str = str.replace(/-/g, ' ');
+    // A post entry.
+    
+    console.log("writeMemberData", JSON.stringify(data))
+	// Get a key for a new Post.
+	
+	var newMemberKey = dbRef.child('members').push().key;
+	
+
+	// db.collection("members").add({
+	// 	data
+	// })
+	// .then(function(docRef) {
+	// 	console.log("Document written with ID: ", docRef.id);
+	// })
+	// .catch(function(error) {
+	// 	console.error("Error adding document: ", error);
+	// });
+
+    dbRef.child('members/' + newMemberKey).update({
+		timestamp: data.timestamp,
+		serial: data.serial,
+		firstname: data.firstname,
+		surname: data.surname,
+		address1: data.address1,
+		address2: data.address2,
+		postcode: data.postcode,
+		phone: data.phone,
+		email: data.email,
+		emailoption: data.emailoption,
+		membershiptype: data.membershiptype,
+		paymenttype: data.paymenttype,
+		trailEndDate: data.trailEndDate
+   	}).then(function(docRef) {
+		console.log("Document written");
+	})
+	.catch(function(error) {
+		console.error("Error adding document: ", error);
+	});
+}
+
+
+
+function addMemberToFB(member) {
+
+	console.log("addMemberToFB");
+
+	
+
+	const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+	member.updateTime(timestamp)
+
+	console.log ("wrtie member to db:", JSON.stringify (member));
  
 	membersRef.push(membership, function () {
     console.log("data has been inserted");
 });
 
 }
-
-
-var callbackAdded = function(snap) {
-    
-	console.log("callbackAdded");
-
-	var row = snap.val();
-
-	console.log("row from db:", snap.key);
-	console.log("row from db:", row);
-
-	//var dataRow = buildLicenseRow(snap.key, snap.val());
-//	var license = new License(row.notValidBefore,row.notValidAfter ,snap.key,row.customer,row.serial, row.hcf,row.maintDate,row.state, row.version,row.s2s,row.wrm);
-
-	//var table = $('#example').DataTable ();
-	//table.rows.add([license]).draw();
-
-};
-
-
-
-
-
 
 // --------------------------
 // DELETE
--- fb/membership.js
+++ fb/membership.js
@@ -2,15 +2,27 @@
 
 class Membership{
 
-    constructor(firstName, lastName, address1,address2,postcode,email,phone,membershipType,endDate){
-        this.firstName = firstName, 
-        this.lastName = lastName, 
-        this.address1 = address1,
-        this.address2 = address2,
-        this.postcode = postcode,
-        this.email = email,
-        this.phone = phone,
-        this.membershipType = membershipType,
-        this.endDate = endDate
+
+    
+
+    constructor(timestamp, serial, firstname, surname, address1, address2, postcode, phone,	email, email_option, membership_type, payment_type, trialEndDate){
+        this.timestamp = timestamp , 
+        this.serial = serial, 
+        this.firstname = firstname, 
+        this.surname = surname , 
+        this.address1 = address1, 
+        this.address2 = address2, 
+        this.postcode = postcode, 
+        this.phone = phone,	
+        this.email = email, 
+        this.email_option = email_option, 
+        this.membership_type = membership_type, 
+        this.payment_type = payment_type, 
+        this.trialEndDate = trialEndDate  
     }
+
+    updateTime(timestamp) {
+        this.lastUpdate = timestamp;
+      }
+    
 }
